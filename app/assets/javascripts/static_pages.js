1e46b5d10cdff9af4cb45709740aa2bc7d5d0c44
apalzab <apalzab@gmail.com>
2014-04-18 19:27:39 +0200

    navigation added

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,6 +1,24 @@
 $(document).ready(function() {
+
   $('.header ul li').click(function() {
     var scroll_to = $(this).data('scroll');
      $('html,body').animate({scrollTop: $(scroll_to).position().top}, 2500);
   });
+
+  $('#sign-in-nav').click(function() {
+      $('.new-user').modal('show');
+  });
+
+  $('#sign-in-bottom').click(function() {
+      $('.new-user').modal('show');
+  });
+
+  $('#login-nav').click(function() {
+      $('.login').modal('show');
+  });
+
+  $('#login-bottom').click(function() {
+      $('.login').modal('show');
+  });
+
 });
