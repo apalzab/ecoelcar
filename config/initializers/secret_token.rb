d28e803e8f86a39bbd397da83e5f0744ca12d8f5
apalzab <apalzab@gmail.com>
2014-04-18 17:57:06 +0200

    heroku config

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ecoelcar::Application.config.secret_key_base = '6349985626842a7b672a82ba7d98f5145d81d62b063ee1b378fa4932a0250e79d34824969eb9d3aa306336cf1288c0246d5b0fa4a16092750c8dd1963fa98dec'
