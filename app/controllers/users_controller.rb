541ad8e04dbf2961d1eca33d3ee4db9542ca4eda
apalzab <apalzab@gmail.com>
2014-05-07 15:20:16 +0200

    user profile can only be changed by the owner

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,10 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def update
     @user = User.find(params[:id])
     if @user.update_attributes user_params
