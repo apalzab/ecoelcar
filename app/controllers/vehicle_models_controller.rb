27156d35c624311a0f3cc83973f924962e72ff70
apalzab <apalzab@gmail.com>
2014-05-26 20:58:27 +0200

    controller improved

--- app/controllers/vehicle_models_controller.rb
+++ app/controllers/vehicle_models_controller.rb
@@ -1,10 +1,10 @@
 class VehicleModelsController < ApplicationController
   def index
     brand = VehicleBrand.find_by_name(params[:brand_name])
-    if brand
-      respond_to do |format|
-        format.json { render :json => brand.models.all }
-      end
+    if brand.nil?
+      render nothing: true
+    else
+      render json: brand.models
     end
   end
 end
