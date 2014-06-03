f640021f031160d19e5f597185d20adf00d9e812
apalzab <apalzab@gmail.com>
2014-06-04 00:26:11 +0200

    stations map improved

--- app/assets/javascripts/tracks_index.js
+++ app/assets/javascripts/tracks_index.js
@@ -1,5 +1,4 @@
 $('.tracks.index').ready(function () {
-  initialize();
   $('#datetimepicker').datetimepicker({
       minDate: get_min_date(),
       useCurrent: false,
@@ -13,6 +12,8 @@ $('.tracks.index').ready(function () {
 
   $('.routes .search .form-dates .map-link').click(function() {
     $('.white-wrap').hide('slow');
+    $('#map').show();
+    initialize();
   });
 
   function get_min_date() {
@@ -45,10 +46,12 @@ $('.tracks.index').ready(function () {
     // var socket = new google.maps.MarkerImage('static/img/charge_map.png');
     // var user = new google.maps.MarkerImage('static/img/user.png');
 
+    infowindows = [];
+
     // Adding markers to the map
-    get_pick_up_points(function(points) {
-      $(points).each(function(index, point) {
-        var latlng = point.latlng;
+    get_pick_up_points(function(stations) {
+      $(stations).each(function(index, station) {
+        var latlng = station.latlng;
         latlng = latlng.split(',');
         var lat = latlng[0];
         var lon = latlng[1];
@@ -57,9 +60,40 @@ $('.tracks.index').ready(function () {
           map: map,
           // icon: socket
         });
+
+        var marker_content =
+          '<div>' +
+            '<h4>' + station.name + '</h4>' +
+            '<p><strong>Dirección:</strong></p>' +
+            '<p><strong>Cómo llegar hasta ahi</strong></p>' +
+            '<select class="span2">' +
+                      '<option>Caminando</option>' +
+                      '<option>Coche</option>' +
+                      '<option>Transporte público</option>' +
+                      '<option>Bicicleta</option>' +
+            '</select>' +
+            '<p><input type = "checkbox" id = "severalRoutes"' +
+            '<label for = "severalRoutes">Mostrar varias rutas</label></p>' +
+            '<button class="btn btn-inverse" data-latitude="43.271026" data-longitude="-2.938458">¿Cómo llegar hasta ahí?</button>' +
+          '</div>';
+
+        var infowindow = new google.maps.InfoWindow();
+        infowindow.setContent(marker_content);
+        infowindows.push(infowindow);
+
+        google.maps.event.addListener(marker, 'click', function() {
+          closeInfoWindows();
+          infowindow.open(map, marker);
+        });
       });
     });
     
+  }
+
+  function closeInfoWindows() {
+    for (i = 0; i < infowindows.length; i++ ) {
+      infowindows[i].close();
+    }
   }
 
   function get_pick_up_points(callback) {
