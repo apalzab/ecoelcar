eabcd3667fe0b9e41530fb7f8c0a2de62b805424
apalzab <apalzab@gmail.com>
2014-06-03 22:28:35 +0200

    adding pick up points map

--- app/assets/javascripts/tracks_index.js
+++ app/assets/javascripts/tracks_index.js
@@ -1,4 +1,5 @@
 $('.tracks.index').ready(function () {
+  initialize();
   $('#datetimepicker').datetimepicker({
       minDate: get_min_date(),
       useCurrent: false,
@@ -8,6 +9,10 @@ $('.tracks.index').ready(function () {
 
   $('.input-group .date').click(function() {
     $('.input-group-addon').click();
+  });
+
+  $('.routes .search .form-dates .map-link').click(function() {
+    $('.white-wrap').hide('slow');
   });
 
   function get_min_date() {
@@ -21,4 +26,39 @@ $('.tracks.index').ready(function () {
     $('#datetime').val(date);
   });
 
+  function initialize() {
+
+    var mapOptions = {
+      zoom: 13,
+      center: new google.maps.LatLng(43.257143,-2.924865),
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      mapTypeControl: true,
+      mapTypeControlOptions: {
+        position: google.maps.ControlPosition.BOTTOM
+      }
+    };
+    map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+    var trafficLayer = new google.maps.TrafficLayer();
+    trafficLayer.setMap(map);
+
+    // var socket = new google.maps.MarkerImage('static/img/charge_map.png');
+    // var user = new google.maps.MarkerImage('static/img/user.png');
+
+      // Adding markers to the map
+    var deusto_marker = new google.maps.Marker({
+        position: new google.maps.LatLng(43.271026,-2.938458),
+        map: map,
+        // icon: socket
+    });
+  }
+
+  // function loadScript() {
+  //   var script = document.createElement("script");
+  //   script.type = "text/javascript";
+  //   script.src = "http://maps.googleapis.com/maps/api/js?key=AIzaSyBmNZpoxu8WGGluEbJ1Q4cKChFN83z-ecU&sensor=false&callback=initialize&libraries=places";
+  //   document.body.appendChild(script);
+  // }
+
 });
+
